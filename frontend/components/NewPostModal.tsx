858b409183d82915a51836b976dd8407554e265f
fernlefr <77874835+fernlefr@users.noreply.github.com>
2022-02-25 20:36:12 -0500

    Update NewPostModal.tsx

--- frontend/components/NewPostModal.tsx
+++ frontend/components/NewPostModal.tsx
@@ -6,23 +6,51 @@ import {
     ModalContent,
     ModalFooter,
     ModalHeader,
-    ModalOverlay
+    ModalOverlay,
 } from "@chakra-ui/modal";
 import {Button, Input, Textarea, VStack} from "@chakra-ui/react";
 import axios from "axios";
+import { Divider } from '@chakra-ui/react'
+import { Container } from '@chakra-ui/react'
 
 interface Props {
     isOpen: boolean;
     onClose: () => void;
 }
 
-const NewPostModal: FC<Props> = ({isOpen, onClose}) => {
-    // TODO: Fill out this handleSubmit function!
-    function handleSubmit(e: any) {
-    }
 
-    // TODO: Implemnt a NewPostModal!
-    return ();
+const NewPostModal: FC<Props> = ({isOpen, onClose}) => {
+  function handleSubmit(e: any) {
+      e.preventDefault()
+      axios.post('http://localhost:8080/posts', {
+              title: e.target.title.value,
+              body: e.target.body.value,
+              createdAt: new Date(),
+          })
+            .then((response) => {
+                alert("Post created!")
+            })
+            .catch((error) => {
+                  alert("Issue creating post :(")
+            })
+  }
+
+  // TODO: Implement a NewPostModal!
+  return (<Modal isOpen={isOpen} onClose={onClose}>
+  <ModalOverlay />
+  <ModalContent>
+    <ModalCloseButton />
+    <ModalHeader>create a new post</ModalHeader>
+    <Divider />
+    <form onSubmit={handleSubmit}>
+      <Input name="title" placeholder="Title" type="text" required />
+      <Textarea name="body" placeholder="Body" required />
+      <Button type="submit">Create Post</Button>
+      </form>
+  </ModalContent>
+</Modal>
+);
 }
+
 
 export default NewPostModal;
