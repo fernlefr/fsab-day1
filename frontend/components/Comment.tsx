62f7197603bfeff11cedea8603bff83fab690fdf
fernlefr <77874835+fernlefr@users.noreply.github.com>
2022-02-25 20:35:44 -0500

    Update Comment.tsx

--- frontend/components/Comment.tsx
+++ frontend/components/Comment.tsx
@@ -1,14 +1,51 @@
-import {FC} from "react";
-import {Box, Divider, Text} from "@chakra-ui/react";
+import { Box, Button, Text} from '@chakra-ui/react'
+import { useState } from 'react';
+import { Input } from '@chakra-ui/react'
+import {
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+    Center,
+    Square,
+    Circle
+  } from '@chakra-ui/react'
 
-interface Props {
-    content: string;
-    postedAt: Date;
+function Card(){
+    const [isClicked, setIsClicked] = useState(false);
+
+    return <div>
+        
+        <Box
+            maxW ='sm' borderWidth='1px' borderRadius='lg' p={8}>
+                <Text fontSize='5xl'>Dear Blueno</Text>
+                <Text fontSize='sm'>Welcome to Dear Blueno! The space where you can share anonymous thoughts.</Text>
+           
+                <Button colorScheme='blue' onClick={ () => alert("Sorry I didn't have time to properly create elegant web design but I will for future projects!")}>Click me for a Surprise!</Button>
+        </Box>
+       
+        <Box
+          maxW ='sm' borderWidth='1px' borderRadius='lg' p={8}>
+              
+<FormControl>
+  <FormLabel htmlFor='email'>Name</FormLabel>
+  <Input id='email' type='email' />
+  <FormHelperText>Your preferred name.</FormHelperText>
+</FormControl>
+
+<FormControl>
+  <FormLabel htmlFor='email'>Email address</FormLabel>
+  <Input id='email' type='email' />
+  <FormHelperText>We'll never share your email.</FormHelperText>
+</FormControl>
+          </Box>
+    </div>
 }
 
-const Comment: FC<Props> = ({content, postedAt}) => {
-    // TODO: Implemnt a Comment!
-    return ();
-}
+ 
+//arrow function makes sure 
 
-export default Comment;
+export default Card;
+
+
+  
