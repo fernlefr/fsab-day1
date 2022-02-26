655dbf01a42e937ecb89502ad3b1c2bc8eb1c995
fernlefr <77874835+fernlefr@users.noreply.github.com>
2022-02-25 20:37:36 -0500

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,10 +3,15 @@ import {Button, Container, HStack, Spacer, Text, VStack} from "@chakra-ui/react"
 import Post from "../components/Post";
 import NewPostModal from "../components/NewPostModal";
 import {useState} from "react";
+import Card from '../components/Card';
+import Comment from '../components/Comment';
 
+//homepage component
 const Home: NextPage = () => {
-    // TODO: Implement this NextPage!
-    return ();
-}
 
-export default Home
+    
+   return <Card/>;
+    // TODO: Implement this NextPage!
+    //return (<Post title="hi" body="hi" postedAt={new Date()}></Post>)
+}
+export default Home;
